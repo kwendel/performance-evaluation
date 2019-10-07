ebe32a91651753110aba4f0156c0e8acccee6b48
Martijn Janssen <martijn9612@gmail.com>
2019-10-07 11:12:52 +0200

    remove sleep from istio installation script

--- scripts/deploy-cluster.sh
+++ scripts/deploy-cluster.sh
@@ -3,8 +3,14 @@
 kubectl create namespace istio-system
 
 helm template istio-1.3.1/install/kubernetes/helm/istio-init --name istio --namespace istio-system --values=istio-1.3.1/install/kubernetes/helm/istio/values-istio-demo.yaml | kubectl apply -f -
-kubectl get crds | grep 'istio.io' | wc -l
 
-sleep 20
+CRDS=""
+
+while [ "$CRDS" != "23" ]; do
+    CRDS=$(kubectl get crds | grep 'istio.io' | wc -l)
+    echo "Waiting for CRDs to be ready";
+    sleep 3
+done
+echo "CRDs are ready"
 
 helm template istio-1.3.1/install/kubernetes/helm/istio --name istio --namespace istio-system --values=istio-1.3.1/install/kubernetes/helm/istio/values-istio-demo.yaml | kubectl apply -f -
