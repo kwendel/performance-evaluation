31eb46bdd6c68acc24e37d65a412608e8e875c0c
Martijn Janssen <martijn9612@gmail.com>
2019-10-07 18:14:07 +0200

    forward kiali port to default kiali port

--- scripts/kiali-portforward.sh
+++ scripts/kiali-portforward.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Forwarding kiali to http://localhost:8080"
+echo "Forwarding kiali to http://localhost:20001"
 echo ""
-kubectl port-forward -n istio-system $(kubectl get pods -l app=kiali -n istio-system -o jsonpath="{.items[0].metadata.name}") 8080:20001
+kubectl port-forward -n istio-system $(kubectl get pods -l app=kiali -n istio-system -o jsonpath="{.items[0].metadata.name}") 20001:20001
