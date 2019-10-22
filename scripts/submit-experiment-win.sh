51f5279f7bb4277c7324a98072a546eb28317ae2
Kasper Wendel <kasperwendel@gmail.com>
2019-10-22 16:47:16 +0200

    script for repeating test + result parser

---
+++ scripts/submit-experiment-win.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Spark options
+SPARK_MASTER="spark://spark-master:7077"
+# The total cores of the Spark Cluster that are going to be used for this task
+# Note: if you claim to many CPUS, the task will be started but with 0 allocated CPUS
+TOTAL_CORES=1
+# The CPUS and Memory per node in the Spark Cluster, that is going to be used for this task
+EXECUTOR_CORES=1
+EXECUTOR_MEMORY=1g
+
+# Python run options
+# https://github.com/intel-analytics/BigDL/tree/master/pyspark/bigdl/models/lenet
+PYTHON_MAIN="/opt/src/lenet5.py"
+# Batch size must be divisible by total_cores into integer
+PYTHON_ARGS="--action train --dataPath /opt/data --batchSize 128 --endTriggerType epoch --endTriggerNum 3"
+
+# Note: docker volumes on Windows require full absolute path..
+EXPERIMENT_NAME="3epoch"
+
+for i in {1..10}; do
+    fname="logs/"${EXPERIMENT_NAME}_${i}".log"
+
+    docker.exe run --rm --network spark-cluster-network \
+           --name spark-submit \
+           -e SPARK_MASTER=${SPARK_MASTER} \
+           -e TOTAL_CORES=${TOTAL_CORES} \
+           -e EXECUTOR_CORES=${EXECUTOR_CORES} \
+           -e EXECUTOR_MEMORY=${EXECUTOR_MEMORY} \
+           -p 4040:4040 \
+           spark-cluster/spark:latest \
+           /opt/submit.sh ${PYTHON_MAIN} ${PYTHON_ARGS} > ${fname}
+done
