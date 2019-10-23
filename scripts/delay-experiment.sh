11faf60ec6a1477f182ff39900704bd667e3722f
Kasper Wendel <kasperwendel@gmail.com>
2019-10-23 16:49:20 +0200

    script for running delay experiment

---
+++ scripts/delay-experiment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+workers=( 1 2 4 6)
+delays=( 10 35 140 500)
+
+for ((i=0;i<${#workers[@]};++i)); do
+    for ((j=0;j<${#delays[@]};++j)); do
+        # Start delay
+        for ((w=1;w<=${workers[i]};w++)); do
+            ./pumba netem --duration 300m delay --time ${delays[j]} containers performance_evaluation_spark-worker_${w} &
+        done
+
+        # Start experiment
+        name="delay-w${workers[i]}-d${delays[j]}"
+        echo ${name}
+        ./scripts/submit-experiment.sh ${name} ${workers[i]}
+
+        # Kill delays
+        pkill -f 'pumba'
+    done
+done
+
--- scripts/submit-experiment.sh
+++ scripts/submit-experiment.sh
@@ -2,11 +2,14 @@
 
 # SETTINGS
 
+EXPERIMENT_NAME=$1
+TOTAL_CORES=$2
+
+
 # Spark options
 SPARK_MASTER="spark://spark-master:7077"
 # The total cores of the Spark Cluster that are going to be used for this task
 # Note: if you claim to many CPUS, the task will be started but with 0 allocated CPUS
-TOTAL_CORES=1
 # The CPUS and Memory per node in the Spark Cluster, that is going to be used for this task
 EXECUTOR_CORES=1
 EXECUTOR_MEMORY=1g
@@ -15,18 +18,19 @@ EXECUTOR_MEMORY=1g
 # https://github.com/intel-analytics/BigDL/tree/master/pyspark/bigdl/models/lenet
 PYTHON_MAIN="/opt/src/lenet5.py"
 # Batch size must be divisible by total_cores into integer
-PYTHON_ARGS="--action train --dataPath /opt/data --batchSize 128 --endTriggerType epoch --endTriggerNum 1"
+PYTHON_ARGS="--action train --dataPath /opt/data --batchSize 120 --endTriggerType epoch --endTriggerNum 20"
 
-EXPERIMENT_NAME="test"
-REPEATS=1
+
+REPEATS=5
 
 # RUN
 LOG_DIR="logs/${EXPERIMENT_NAME}/"
 
 echo "Starting with options:"
-echo "Py Main: ${PYTHON_MAIN}"
-echo "Args: ${PYTHON_ARGS}"
+#echo "Py Main: ${PYTHON_MAIN}"
+#echo "Args: ${PYTHON_ARGS}"
 echo "Log directory: ${LOG_DIR}"
+echo "Worker cores: ${TOTAL_CORES}"
 
 if [[ -d "$LOG_DIR" ]]; then
     echo "Log directory already exists, change EXPERIMENT_NAME variable"
@@ -40,15 +44,15 @@ fi
 for i in $(eval echo "{1..${REPEATS}}"); do
     fname=${LOG_DIR}${i}".log"
 
-    docker.exe run --rm --network spark-cluster-network \
-           --name spark-submit \
-           -e SPARK_MASTER=${SPARK_MASTER} \
-           -e TOTAL_CORES=${TOTAL_CORES} \
-           -e EXECUTOR_CORES=${EXECUTOR_CORES} \
-           -e EXECUTOR_MEMORY=${EXECUTOR_MEMORY} \
-           -p 4040:4040 \
-           spark-cluster/spark:latest \
-           /opt/submit.sh ${PYTHON_MAIN} ${PYTHON_ARGS} > ${fname}
+#    docker.exe run --rm --network spark-cluster-network \
+#           --name spark-submit \
+#           -e SPARK_MASTER=${SPARK_MASTER} \
+#           -e TOTAL_CORES=${TOTAL_CORES} \
+#           -e EXECUTOR_CORES=${EXECUTOR_CORES} \
+#           -e EXECUTOR_MEMORY=${EXECUTOR_MEMORY} \
+#           -p 4040:4040 \
+#           spark-cluster/spark:latest \
+#           /opt/submit.sh ${PYTHON_MAIN} ${PYTHON_ARGS} > ${fname}
 
     echo "Finished [${i}/${REPEATS}]"
 done
