bb3f3e86af69aeca1daae0e75f46894015253be4
Kasper Wendel <kasperwendel@gmail.com>
2019-10-23 22:14:23 +0200

    remove logs from cloud branch

--- scripts/install-compute.sh
+++ scripts/install-compute.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sudo apt update
-apt install -y docker docker-compose
+sudo apt update && apt install -y docker docker-compose
 
-gcloud compute scp --project "qpe-project-254709" --zone "europe-west4-b" performance-evaluation/ instance-1:~/performance-evalution/ --recurse
+#gcloud compute scp --project "qpe-project-254709" --zone "europe-west4-b" performance-evaluation/ instance-1:~/performance-evalution/ --recurse
 
-wget https://github.com/alexei-led/pumba/releases/download/0.6.5/pumba_linux_amd64
-mv pumba_linux_amd64 pumba
+wget https://github.com/alexei-led/pumba/releases/download/0.6.5/pumba_linux_amd64 && mv pumba_linux_amd64 pumba
 
 sudo usermod -aG docker $USER
 sudo reboot
