28e2ce9fdcd681b248ad45684cd26f30b8205040
Kasper Wendel <kasperwendel@gmail.com>
2019-10-27 12:15:35 +0100

    Revert "remove logs from cloud branch"
    This reverts commit bb3f3e86af69aeca1daae0e75f46894015253be4.

--- scripts/install-compute.sh
+++ scripts/install-compute.sh
@@ -5,7 +5,9 @@ exit
 
 sudo apt update && sudo apt install -y docker docker-compose
 
-#git clone https://github.com/kwendel/performance-evaluation.git && cd performance-evaluation
+git clone https://github.com/kwendel/performance-evaluation.git && cd performance-evaluation
+
+# gcloud compute scp --project "qpe-project-254709" --zone "europe-west4-b" performance-evaluation/ instance-1:~/performance-evalution/ --recurse
 
 wget https://github.com/alexei-led/pumba/releases/download/0.6.5/pumba_linux_amd64 && mv pumba_linux_amd64 pumba && chmod +x pumba
 
